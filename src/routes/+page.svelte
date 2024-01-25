b8147fd9458774f084fffa769cacb041588ac435
teo-clone <theodore.tsivranidis@gmail.com>
2024-01-25 17:08:16 -0500

    abandoning google places api for a sec, leaving it commented out in app.html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import BarSearch from '$lib/components/BarSearch.svelte';
+
+	import { createSearchStore, searchHandler } from '$lib/stores/search';
+	import { onDestroy } from 'svelte';
 
 	export let data: PageData;
+
+	const searchableBars = data.bars.map((bar) => ({
+		...bar,
+		searchTerms: `${bar.name} ${bar.address}`
+	}));
+
+	const searchStore = createSearchStore(searchableBars);
+
+	const unsubscribe = searchStore.subscribe((model) => searchHandler(model));
+
+	onDestroy(() => unsubscribe());
 </script>
 
 <a href="/"><h1>bathroom <br /> wall</h1></a>
@@ -11,4 +24,19 @@
 	night
 </p>
 
-<BarSearch currentBars={data.bars} />
+<form class="flex flex-col">
+	<input
+		placeholder="search current bars by name or address..."
+		type="text"
+		bind:value={$searchStore.search}
+	/>
+</form>
+
+<div class="flex flex-col gap-[20px] border border-gray-500 bg-white p-2">
+	{#each $searchStore.filtered as bar}
+		<a href={`/bar/${bar.id}`}>
+			<h2>{bar.name}</h2>
+			<p class="text-sm">{bar.address}</p>
+		</a>
+	{/each}
+</div>
