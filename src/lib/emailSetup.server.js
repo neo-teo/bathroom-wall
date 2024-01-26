5beed5e6e8e5ef0cfc5b30cf5202bb4b2111a9ac
teo-clone <theodore.tsivranidis@gmail.com>
2024-01-26 11:45:27 -0500

    added pool: true

--- src/lib/emailSetup.server.js
+++ src/lib/emailSetup.server.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 import { GOOGLE_EMAIL, GOOGLE_EMAIL_PASSWORD } from "$env/static/private";
 
 let transporter = nodemailer.createTransport({
+    pool: true,
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
