1fbb54c6734ec73f1fc51d33840391ef662b3912
teo-clone <theodore.tsivranidis@gmail.com>
2024-03-02 18:42:44 -0500

    replaced text-nowrap with whitespace-nowrap

--- src/lib/components/ActivityIndicatorLegend.svelte
+++ src/lib/components/ActivityIndicatorLegend.svelte
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center gap-2 text-sm text-gray-400">
-	<div class="text-nowrap">less activity</div>
+	<div class="whitespace-nowrap">less activity</div>
 	<div class="h-2 w-[fill] rounded border bg-gradient-to-r from-white via-blue-100 to-blue-500" />
-	<div class="text-nowrap">more activity</div>
+	<div class="whitespace-nowrap">more activity</div>
 </div>
