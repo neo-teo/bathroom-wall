6016217bbe1c66ec490c2f3bd0f808d0faa5cad1
teo-clone <theodore.tsivranidis@gmail.com>
2024-10-28 20:35:51 -0400

    added iphone favicon

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -9,11 +9,17 @@
 
 <div class="grid h-10 grid-cols-[33.33%_auto]">
 	<div class="flex bg-black">
-		<a class="h-full flex items-center text-center bg-gray-900 px-1 text-xl font-black text-white no-underline" href="/">bath</a>
-		<a class="h-full flex items-center bg-white px-1 text-xl font-black no-underline" href="/"
+		<a
+			class="flex h-full items-center bg-gray-900 px-1 text-center text-xl font-black text-white no-underline"
+			href="/">bath</a
+		>
+		<a class="flex h-full items-center bg-white px-1 text-xl font-black no-underline" href="/"
 			>wall</a
 		>
-		<a class="h-full flex items-center bg-gray-900 px-1 text-xl font-black text-white no-underline" href="/">.co</a>
+		<a
+			class="flex h-full items-center bg-gray-900 px-1 text-xl font-black text-white no-underline"
+			href="/">.co</a
+		>
 	</div>
 
 	{#if barName}
@@ -23,8 +29,8 @@
 			</div>
 		</div>
 	{:else}
-		<div class="flex items-center justify-end px-2 py-1 text-right text-xs italic font-medium">
-			leave your mark on the bathroom wall 🤙
+		<div class="flex items-center justify-end px-2 py-1 text-right text-xs font-medium italic">
+			leave your mark on the bathroom wall
 		</div>
 	{/if}
 </div>
